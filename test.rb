728a17919d54ddc0f58e99266473a6a564995d0f
Georgios Romanas <giorgos.romanas@camelotglobal.com>
2020-05-19 09:52:16 +0300

    initial commit

---
+++ test.rb
@@ -0,0 +1,14 @@
+require 'watir'
+browser = Watir::Browser.new :firefox, url: "http://hub:4444/wd/hub"
+
+browser.goto 'google.com'
+search_field = browser.text_field name: 'q'
+search_field.set 'Hello World!'
+browser.send_keys :enter
+
+# Just to see the browser
+sleep 5
+
+puts browser.title
+# => 'Hello World! - Google Search'
+browser.quit 
